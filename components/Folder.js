d0a051c6ac2435d3191df74da617d060d2e57f91
racheltgunawan <58867074+racheltgunawan@users.noreply.github.com>
2024-03-11 14:51:28 -0700

    Add files via upload

---
+++ components/Folder.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Folder = () => {
+    return (
+        <View>
+            <Text>This is a folder</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    
+})
+
+export default Folder;
