5089998660fa5fb607a25a0523fba965ecb8fe0c
tien <len8@spu.edu>
2024-03-13 15:35:02 -0700

    update file

--- App.js
+++ App.js
@@ -1,60 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Task from './components/Task'
-import Folder from './components/Folder'
+import 'react-native-gesture-handler';
+import Inventory from './screens/Inventory';
+import Shop from './screens/Shop';
+import HomeScreen from './screens/HomeScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import {open} from 'react-native-quick-sqlite';
-// const db = open('myDb.sqlite');
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.upComingTasksWrapper}>
-        <Text style={styles.upComingTasks}>Upcoming Tasks:</Text>
-
-        <View style={styles.Tasks}>
-          <Task/>
-          <Task/>
-        </View>
-      </View>
-      <View style={styles.foldersWrapper}>
-        <View style={styles.folder}>
-          <Folder/>
-          <Folder/>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Inventory" component={Inventory} />
+          <Drawer.Screen name="Shop" component={Shop} />
+        </Drawer.Navigator>
+      </NavigationContainer>   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column', // Arrange children vertically
-    backgroundColor: '#fff',
-  },
-  upComingTasksWrapper: {
-    flex: 0.5, // Takes up half of the vertical space
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  upComingTasks: {
-    fontWeight: 'bold',
-    fontSize: 20
-  },
-  Tasks: {
-    
-  },
-  foldersWrapper: {
-    flex: 1, // Takes up the remaining space
-    flexDirection: 'row', // Display children horizontally
-    justifyContent: 'space-between', // Space between columns
-    paddingHorizontal: 30, // Optional: Add padding to the sides
-  },
-  folder: {
-    flex: 1, // Each column takes up equal space
-    paddingHorizontal: 5, // Optional: Add padding between columns
-  },
-});
